5b12870386ebe42188af5ce53776003a2712f277
Rituraj Kulshresth <idohaveemailid@gmail.com>
2024-03-05 02:38:27 +0530

    fd

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3100;
+const PORT = 80;
 
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
